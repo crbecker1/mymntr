027aa2560d4b0db2792fabdce747bf713db7102e
Heather Moore <pair+heather@citizencode.io>
2015-08-31 13:30:41 -0700

    update smtp port settings

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
-    :port      => 25,
+    :port      => 587,
     :enable_starttls_auto => true,
     :user_name => ENV["MANDRILL_USERNAME"],
     :password  => ENV["MANDRILL_API_KEY"],
