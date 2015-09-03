bcadf5745b0e880bc8f9374c9be3668a11994b7c
Heather Moore and Heather Perry <pair+heather+heat@citizencode.io>
2015-09-03 12:44:53 -0700

    only subscribe new users

--- app/jobs/subscribe_user_to_list_job.rb
+++ app/jobs/subscribe_user_to_list_job.rb
@@ -4,10 +4,15 @@ class SubscribeUserToListJob < ActiveJob::Base
   def perform(account, result_string)
     list = ENV["MAILCHIMP_#{result_string.upcase}_LIST_ID"]
     gb = Gibbon::Request.new
-    gb.lists(list).members.create(
-      body: {
-        email_address: account.email,
-        status: "subscribed"
-      })
+    subscribed_emails = gb.lists(list).members.retrieve['members'].map { |m|
+      m['email_address']
+    }
+    unless subscribed_emails.include?(account.email)
+      gb.lists(list).members.create(
+        body: {
+          email_address: account.email,
+          status: "subscribed"
+        })
+    end
   end
 end
