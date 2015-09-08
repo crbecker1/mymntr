f79dbce90e3edf3514b9a6114068d74a289b94b9
Heather Moore and Heather Perry <pair+heather+heat@citizencode.io>
2015-09-08 12:09:53 -0700

    update fb share image to one with better aspect ratio

--- app/views/layouts/raw.rb
+++ app/views/layouts/raw.rb
@@ -7,7 +7,8 @@ class Views::Layouts::Raw < Views::Base
         meta(:charset => "utf-8")
         meta(:content => "width=device-width, initial-scale=1.0", :name => "viewport")
         meta(:content => Rails.application.config.project_description, :name => "description")
-        meta(:content => asset_url('mymntr-hero.jpg'), :name => "og:image")
+        meta(:content => asset_url('screaming-hero-medium.jpg'), :name =>
+            "og:image")
 
         title(content_for?(:title) ? yield(:title) : Rails.application.config.project_name)
 
