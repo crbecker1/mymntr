d97362684a9d7f46e4524bf65fb78786c87d68fd
Heather Moore and Heather Perry <pair+heather+heat@citizencode.io>
2015-09-08 10:38:23 -0700

    link to video from fearless

--- app/views/shared/profiles/fearless.html.rb
+++ app/views/shared/profiles/fearless.html.rb
@@ -8,9 +8,13 @@ How do you do that? Have a way of winning the prize, gathering a crowd, and brea
 records while still being a friend? Oh, you can focus like a laser beam – to the degree
 that just thinking about it SO MUCH seems to make people do it? Wow, all that and people
 want to have a beer with you – that is, if you're of age! Or, watch you do it!"
-    p "You are also a lot like world famous game designer Lorne Lanning! Some of his
+    p {
+      text "You are also a lot like world famous game designer Lorne Lanning! Some of his
 Superpowers are intense perceptual expertise, battling fear, and creating something out
-of nothing! Sound like you? Watch the video with Lorne and Kevin to remind yourself how
-great a brain you have."
+of nothing! Sound like you? "
+      link_to "Watch the video with Lorne and Kevin", 'https://www.youtube.com/watch?v=00q71VOOu2s', class: 'link-copy', target:
+          '_blank'
+      text " to remind yourself how great a brain you have."
+    }
   end
 end
